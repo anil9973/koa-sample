d53a641b2d877bdfbf6a44e03e1cca68491ff9e9
anilf01 <anilf1f@gmail.com>
2022-04-30 23:07:40 +0530

    first commit

--- config/azure.js
+++ config/azure.js
@@ -1,4 +1,4 @@
-import { BlobServiceClient, StorageSharedKeyCredential } from "@azure/storage-blob";
+/* import { BlobServiceClient, StorageSharedKeyCredential } from "@azure/storage-blob";
 
 const account = "statichome";
 const accountKey = "xns3OZxbuz8ACLZwijwkPrIDebr+fT0VqKYeAtdkD/U6oo96YjnD0cYOPMxa6L3EcAD5aXouttQw4w4DkdP6fA==";
@@ -10,26 +10,25 @@ const blobServiceClient = new BlobServiceClient(
 );
 
 const containerName = "static-studio";
-const containerClient = blobServiceClient.getContainerClient(containerName);
+const containerClient = blobServiceClient.getContainerClient(containerName); */
 
-export const uploadBlob = async (file) => {
-	const blobName = file.originalname;
-	const blockBlobClient = containerClient.getBlockBlobClient(blobName);
-	await blockBlobClient.uploadData(file.buffer);
-	return blobName;
-};
+// export const uploadBlob = async (file) => {
+// 	const blobName = file.originalname;
+// 	const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+// 	await blockBlobClient.uploadData(file.buffer);
+// 	return blobName;
+// };
 
 //! for base64 to buffer
-/* const uploadTxtURL = async (text64, fileName) => {
-	const blobName = getBlobName(fileName);
+/* export const uploadTxtURL = async (text64, blobName) => {
 	const blockBlobClient = containerClient.getBlockBlobClient(blobName);
 	const buffer = Buffer.from(text64, "base64");
 
 	await blockBlobClient.uploadData(buffer);
 	return blobName;
-};
+}; */
 
-const uploadURL = async (pdfUrl, pdfName) => {
+/*const uploadURL = async (pdfUrl, pdfName) => {
 	const blockBlobClient = pdfContainerClient.getBlockBlobClient(pdfName);
 	await blockBlobClient.syncUploadFromURL(pdfUrl);
 	return pdfName;
--- server.js
+++ server.js
@@ -1,48 +1,10 @@
 import Koa from "koa";
-import multer from "@koa/multer";
-import { uploadBlob } from "./config/azure.js";
+// import { uploadTxtURL } from "./config/azure.js";
 
 const app = new Koa();
 
-const multerMid = multer({
-	limits: {
-		fileSize: 1024 * 1024,
-	},
-});
-
 app.use(async (ctx, next) => {
-	ctx.method != "POST" && ctx.throw(403, "forbidden request");
-	/* 	const token = ctx.cookies.get("TIDE");
-	token ?? ctx.throw(401, "Not Authenticated");
-
-	let decodedToken;
-
-	try {
-		decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-	} catch (error) {
-		error.statusCode = 500;
-		throw error;
-	}
-	if (!decodedToken) ctx.throw(401, "Not Authenticated"); */
-
-	await next();
-});
-
-app.use(multerMid.single("image"));
-const EXT_ID = "chrome-extension://pojmlllkhfjlhaagafkbfclmbhjknppp";
-
-app.use(async (ctx, next) => {
-	//check origin
-	const origin = ctx.headers.origin;
-	if (!origin || origin != EXT_ID) ctx.throw(403, "forbidden request");
-
-	//get image
-	const image = ctx.request.file;
-	if (!image) ctx.throw(400, "image required");
-
-	//upload image to azure
-	const imgUrl = await uploadBlob(image);
-	ctx.body = imgUrl;
+	ctx.body = { pong: "it worked" };
 });
 
 app.listen(2000, () => console.log("node started"));
